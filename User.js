642bbec0a7ea8f70b520cd6560299d4fb50a19b4
Jesús David Caballero Rocha <69738744+zhentinela-ai@users.noreply.github.com>
2022-04-03 07:38:04 +0000

    Initial commit

---
+++ User.js
@@ -0,0 +1,6 @@
+export class User {
+  constructor(name, lastname) {
+    this.name = name
+    this.lastname = lastname
+  }
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import {User} from './User.js'
+
+const ryan = new User("Ryan", "Ray")
+
+console.log(ryan)
